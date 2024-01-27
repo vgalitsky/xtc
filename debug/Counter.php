f2faeb8ec829e9819d14d237fb5500130a79a298
vgalitsky <concept.galitsky@gmail.com>
2024-01-27 16:11:26 +0200

    debug: added counter

---
+++ debug/Counter.php
@@ -0,0 +1,56 @@
+<?php
+namespace XTC\Debug;
+
+class Counter
+{
+    /**
+     * The counters container
+     *
+     * @var array
+     */
+    protected static $counters = [];
+
+    /**
+     * Increment a counter
+     *
+     * @param string $name Counter name
+     * 
+     * @return void
+     */
+    public static function increment(string $name): void
+    {
+        if (!isset(static::$counters[$name])) {
+            static::$counters[$name] = 0;
+        }
+
+        static::$counters[$name]++;
+    }
+
+    /**
+     * Decrement a counter
+     *
+     * @param string $name The counter name
+     * 
+     * @return void
+     */
+    public static function decrement(string $name): void
+    {
+        if (!isset(static::$counters[$name])) {
+            static::$counters[$name] = 1;
+        }
+
+        static::$counters[$name]--;
+    }
+
+    /**
+     * Get the counter value
+     *
+     * @param string $name The counter name
+     * 
+     * @return integer
+     */
+    public static function get(string $name): int
+    {
+        return static::$counters[$name];
+    }
+}
